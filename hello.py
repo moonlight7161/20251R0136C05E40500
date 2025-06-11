25f2ab4339fb5ba0f52a9269932695084f7dfbc4
moonlight7161 <anseodbs3@korea.ac.kr>
2025-06-11 16:59:05 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,13 @@ def is_even(n):
     return n % 2 == 0
 
 print(is_even(4))  # True
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+print(bubble_sort([5, 1, 4, 2, 8]))
